c6609acfc0822212b7053b789d99243d8290793c
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-14 14:30:04 +0700

    fix: allow Market Deal API from GitHub Pages

--- api/market.js
+++ api/market.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Content-Type');
+
+  if (req.method === 'OPTIONS') return res.status(204).end();
   if (req.method !== 'GET') {
     res.setHeader('Allow', 'GET');
     return res.status(405).json({ error: 'Method not allowed' });
